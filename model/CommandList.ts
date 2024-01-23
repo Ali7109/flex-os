97554899c1ce8102bb6e2049564cf6c7e7ff1920
Ali Amin <aliamin745@gmail.com>
2024-01-22 22:39:31 -0500

    Added wait and date command list

--- model/CommandList.ts
+++ model/CommandList.ts
@@ -2,26 +2,23 @@ export const CommandList:string[] = [
     "help",
     "what",
     "clear",
+    "wait",
+    "date" 
 ]
 
 export const responseList = new Map<string, string>([
     ["help",  "Here is a list of commands you can try: <br><br>" +
-    "help - displays this list of commands <br>" +
-    "what - displays a description of this terminal emulator <br>" +
-    "clear - clears the terminal screen <br>" +
-    "ls - lists all the files in the current directory <br>" +
-    "cd - changes the current directory <br>" +
-    "cat - displays the contents of a file <br>" +
-    "mkdir - creates a new directory <br>" +
-    "touch - creates a new file <br>" +
-    "rm - deletes a file or directory <br>" +
-    "mv - moves a file or directory <br>" +
-    "cp - copies a file or directory <br>" +
-    "pwd - displays the current directory <br>" +
-    "history - displays the history of commands <br>" +
-    "date - displays the current date and time <br>" +
-    "exit - exits the terminal emulator"],
+    "<strong>help</strong> - displays this list of commands. <br>" +
+    "<strong>what</strong> - displays a description of what this is. <br>" +
+    "<strong>clear</strong> - clears the terminal screen. <br>" +
+    "<strong>ls</strong> - lists all the objects in the system. <br>" +
+    "<strong>touch</strong> - creates a new object. <br>" +
+    "<strong>wait</strong> - blocks input for some time, usage 'wait <strong>time</strong>' <br>" +
+    "<strong>date</strong> - displays the current date and time. <br>" +
+    "<strong>exit</strong> - exits the terminal emulator."],
     ["what", "This is a terminal emulator that simulates a Linux terminal."],
     ["clear", ""],
+    ["wait", ""],
+    ["date", ""]
 ]);
     
