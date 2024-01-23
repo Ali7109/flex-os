3bf241cd4e04f86ddf6935196e24b02cf7850428
Ali Amin <aliamin745@gmail.com>
2024-01-22 22:39:52 -0500

    Added features to runProcesses method

--- model/CommandProcess.ts
+++ model/CommandProcess.ts
@@ -1,14 +1,56 @@
 import { responseList } from "./CommandList";
+import { getDate } from "./HelperFunctions";
 
-const runProcess = (process: string) => {
-    if (process === "clear") {
-        // need to start implementing processes, firstly clear
-        
+function isNumeric(str:string){
+    let pattern:RegExp  = /^[0-9]+$/;
+    return pattern.test(str)
+}
+
+type ProcessResponse = {
+    type: string,
+    response:string,
+    arg: number
+}
+const runProcess = (processArr: string[]):ProcessResponse => {
+    if (processArr.length === 1){
+
+        if (processArr[0].toLowerCase() === "date"){
+            let date = new Date();
+            return {
+                type: "date",
+                response: getDate(date.getDate(), date.getDay(), date.getFullYear()),
+                arg: -2
+            }
+        }
+
+    } else if (processArr.length === 2){
+        let argument = processArr[1];
+        if(processArr[0].toLowerCase() === "wait"){
+            if (isNumeric(argument) && parseInt(argument) > 0){
+                return {
+                    type: "wait",
+                    response: "Waiting for " + argument + " seconds.",
+                    arg: parseInt(argument)
+                };
+            } else {
+                return {
+                    type: "invalid",
+                    response: "Invalid ; correct usage for 'wait': <strong>wait (time in seconds)</strong>",
+                    arg: -1
+                }
+            }
+        } 
+    }
+    return {
+        type: "invalid",
+        response: "Invalid Command",
+        arg: -1
     }
 }
-export const processInputToCommand = (command: string, myDb:Database, trie:CommandTrie) => {
 
-    let cmdarr: string[] = command.split(" ");
+export const processInputToCommand = (command: string, myDb:Database, trie:CommandTrie):ProcessResponse => {
+
+    let cmdarr: string[] = command.toLowerCase().split(" ");
     let cmd: string = cmdarr[0];
 
     if (cmdarr.length > 0) {
@@ -17,13 +59,22 @@ export const processInputToCommand = (command: string, myDb:Database, trie:Comma
         if (res !== null) {
             let responseToCommand = responseList.get(res);
             if (responseToCommand !== undefined) {
-                if (res === ""){
-                    runProcess(res);
+                if (responseToCommand === ""){
+                    return runProcess(cmdarr);
+                } else {
+                    return {
+                        type: "free",
+                        response: responseToCommand,
+                        arg: 0
+                    }
                 }
-                return responseToCommand; 
             }
         }
     }
-    return "Invalid Command"
+    return {
+        type: "invalid",
+        response: "Invalid Command",
+        arg: -1
+    }
 }
 
