b99ccf130f07b0d33a46fd4555a9cfd24e7eab16
Ali Amin <aliamin745@gmail.com>
2024-01-23 15:36:22 -0500

    Updated database

--- model/DEFINED OBJS/DefinedObjs.ts
+++ model/DEFINED OBJS/DefinedObjs.ts
@@ -1,7 +1,42 @@
 import { ProcessResponse } from "../Types/Types";
 
-export const INVALID_COMMAND:ProcessResponse = {
-    type: "invalid",
-    response: "Invalid command or usage. Please type <strong>help</strong> for a list of commands and usage.",
-    arg: -1
+// For commands not available in the trie
+export const INVALID_COMMAND = (invalidMsg:string):ProcessResponse => {
+
+    let prefix:string = "Invalid command: ";
+    let generalSuffix:string = "Please type <strong>help</strong> for a list of commands and usage.";
+    let suffix:string = (invalidMsg.trim() !== "") ? invalidMsg.trim() : generalSuffix;
+    return {
+        type: "invalid",
+        response: prefix + suffix,
+        arg: -1
+    }
+}
+
+
+// For commands with no argument
+export const FREE_COMMAND = (type:string, responseToCommand:string):ProcessResponse => {
+    return {
+        type,
+        response: responseToCommand,
+        arg: 0
+    }
+}
+
+// For commands with argument
+export const ARG_COMMAND = (type:string, responseToCommand:string, arg:number):ProcessResponse => {
+    return {
+        type,
+        response: responseToCommand,
+        arg
+    }
+}
+
+// System error, db init error or other non input related errors
+export const SYSTEM_ERROR = (error:string):ProcessResponse => {
+    return {
+        type: "system_error",
+        response: ("System error: " + error),
+        arg: -2
+    }
 }
